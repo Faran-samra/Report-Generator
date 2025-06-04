79a4ee8b4082121e7941853fd17bdc8587016dcf
Faran-samra <faransamra45@gmail.com>
2025-06-04 15:46:39 +0500

    Prepare for Heroku deployment

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-const express = require("express");
-const path = require("path");
+// server.js
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from /dist
 app.use(express.static(path.join(__dirname, "dist")));
 
+// All other routes return index.html (SPA)
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
